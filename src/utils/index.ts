4d0ea8bb2307e7fbf9e0f64dbd4318aed06edd75
Metallicus Dev <>
2022-04-08 12:56:16 -0600

    :recycle: Rename files to match structure

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,8 @@
-export * from './details-error';
+export * from './detailsError';
 export * from './integer';
 export * as template from './template';
-export * from './validate-name';
-export * from './file-management';
+export * from './validateName';
+export * from './fileManagement';
 export * from './permissions';
 export * from './prompt';
 export * from './resources';
