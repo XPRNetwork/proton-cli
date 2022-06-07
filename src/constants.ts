6ff3203892ce9a974905be2a63d2380468cc6852
Denis <carriere.denis@gmail.com>
2022-06-06 20:15:36 -0400

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,15 @@
 export const networks = [
 	{chain: 'proton', endpoint: 'https://proton.greymass.com'},
-	{chain: 'proton-test', endpoint: 'https://protontestnet.greymass.com'}
+	{chain: 'proton-test', endpoint: 'https://protontestnet.greymass.com'},
+  {chain: 'eos', endpoint: 'https://eos.greymass.com'},
+  {chain: 'wax', endpoint: 'https://wax.greymass.com'},
+  {chain: 'wax-test', endpoint: 'https://waxtest.api.eosnation.io'},
+  {chain: 'telos', endpoint: 'https://telos.greymass.com'},
+  {chain: 'telos-test', endpoint: 'https://telostest.api.eosnation.io'},
+  {chain: 'ux', endpoint: 'https://ux.api.eosnation.io'},
+  {chain: 'ultra', endpoint: 'https://ultra.api.eosnation.io'},
+  {chain: 'fio', endpoint: 'https://fio.greymass.com'},
+  {chain: 'jungle4-test', endpoint: 'https://jungle4.api.eosnation.io'},
+  {chain: 'kylin-test', endpoint: 'https://kylin.api.eosnation.io'},
+	{chain: 'localhost', endpoint: 'http://localhost:8888'}
 ]
